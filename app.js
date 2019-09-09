fb29b0ee6577409ac58b7d2ea7fed80f59d9cded
nSedrickm <nsedrick101@gmail.com>
2019-09-09 13:29:20 +0100

    reading from db

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'user',
-  password: 'password',
+  user: 'root',
+  password: '',
   database: 'DB'
 });
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected!');
 });
+
+//reading data from the database
+connection.query('SELECT * FROM employees', (err,rows) => {
+  if(err) throw err;
+
+  console.log('Data received from DB employees table:\n');
+  console.log(rows);
+
+  //logging individual output
+  rows.forEach( (row) => {
+  console.log(`${row.name} is in ${row.location}`);
+});
+
+});
+
+
+
