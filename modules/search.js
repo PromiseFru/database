5c0c6df73825ce32f43c3ca4ee9a901b006b3157
Promise Fru <promisefru1@gmail.com>
2019-09-09 13:14:10 +0100

    sort search results fixed, reload button added

--- modules/search.js
+++ modules/search.js
@@ -1,7 +1,11 @@
 import { dataholder } from "./dataholder.js";
+import { sortBtnClicked } from "./buttons.js";
 
 // search through data for item that match
 export function search(data, searchbox) {
-    var search = data.filter(data => data.meta_place === searchbox.value);
+    var search = data.filter(data => {
+        return (data.meta_place === searchbox.value || data.meta_time === searchbox.value || data.meta_date === searchbox.value);
+    });
     dataholder(search);
+    sortBtnClicked(search);
 }
