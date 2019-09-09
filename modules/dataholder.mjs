8285964fd9528cd506405adcc7e460ac10ef2a88
Promise Fru <promisefru1@gmail.com>
2019-09-09 14:57:28 +0100

    fallback to single JS fule: script.js

--- modules/dataholder.mjs
+++
@@ -1,7 +0,0 @@
-import { renderHTML } from "./views.mjs";
-
-// databoard pre-previewed data
-export function dataholder(inputdata) {
-    let container = inputdata;
-    renderHTML(container);
-}
---
+++ script.js
@@ -0,0 +1,123 @@
+$(document).ready(() => {
+
+    //ajax call from ./module/ajax,js
+    mydata('https://raw.githubusercontent.com/PromiseFru/database/master/db.json',
+        data => {
+            // dataholder call from ./module/dataholder.js
+            dataholder(data);
+            // buttons calls from ./modules/buttons.js
+            sortBtnClicked(data);
+            searchbtnclicked(data);
+            // searchgen call from scipts.js
+            searchgen(data);
+        });
+});
+
+// AJAX
+function getJSON(url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(this.responseText)
+    };
+    xhr.open('GET', url, true);
+    xhr.send();
+}
+
+function mydata(url, callback) {
+    getJSON(url, data => callback(JSON.parse(data)));
+}
+
+// DATAHOLDER
+// databoard pre-previewed data
+function dataholder(inputdata) {
+    let container = inputdata;
+    renderHTML(container);
+}
+
+// BUTTONS
+// sort data when button clicked
+function sortBtnClicked(data) {
+    // fetch btn-sort id from HTML
+    var btnSort = document.getElementById('btn-sort');
+
+    btnSort.addEventListener('click', () => {
+        sort(data);
+    });
+}
+
+function searchbtnclicked(data) {
+    var searchbtn = document.getElementById('searchbtn');
+    var searchbox = document.getElementById('searchbox');
+
+    searchbtn.addEventListener('click', () => {
+        search(data, searchbox);
+    })
+}
+
+// SEARCH
+// search through data for item that match
+function search(data, searchbox) {
+    var search = data.filter(data => {
+        return (data.meta_place === searchbox.value || data.meta_time === searchbox.value || data.meta_date === searchbox.value);
+    });
+    dataholder(search);
+    sortBtnClicked(search);
+}
+
+// generate search data
+function searchgen(data) {
+    var dbList = document.getElementById('dblist');
+    var stuff = '';
+
+    for (let i in data) {
+        stuff += `
+        <option value="${data[i].meta_place}">
+        `
+    }
+
+    for (let i in data) {
+        stuff += `
+        <option value="${data[i].meta_time}">
+        `
+    }
+
+    for (let i in data) {
+        stuff += `
+        <option value="${data[i].meta_date}">
+        `
+    }
+    dbList.innerHTML = stuff;
+}
+
+// SORT
+//sort data by given data
+function sort(data) {
+    data.sort((a, b) => {
+        // sort by meta_place alphabetically
+        return a.meta_place.localeCompare(b.meta_place);
+    });
+    dataholder(data);
+}
+
+// VIEWS
+// render HTML
+function renderHTML(data) {
+    // fetching table id in HTML
+    var results = document.getElementById('results');
+
+    var text = '';
+
+    // loop through creating table data for as many objects are present in JSON
+    for (let i in data) {
+        text += `
+        <tr>
+            <td scope="row">${data[i].meta_time}</td>
+            <td>${data[i].meta_place}</td>
+            <td>${data[i].meta_date}</td>
+        </tr>
+        `
+    }
+
+    // Render generated results to ID HTML document
+    results.innerHTML = text;
+};
