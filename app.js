389a0859eae34626d279e592edafcc9b4d45cddd
nSedrickm <nsedrick101@gmail.com>
2019-09-13 20:13:52 +0100

    express output

--- app.js
+++ app.js
@@ -24,4 +24,16 @@ connection.query('SELECT * FROM employees', (err,rows) => {
 
 });
 
+//writing to display with express
+const express = require('express')
+const app = express()
+const path = require('path');
+const router = express.Router();
+const port = 3000
 
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.use('/', router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
